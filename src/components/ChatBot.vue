ef8c40c2269f4d9d785e60ae8be5df5f743be18f
SeonghaPark <rd1581@naver.com>
2019-07-24 16:03:37 +0900

    chatbot.vue 추가

---
+++ src/components/ChatBot.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script>
+  export default {
+	name: 'ChatBot',
+  data () {
+      return {
+
+      }
+  },
+  mounted(){
+
+    },
+  methods: {
+ 
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,37 +1,38 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HomePage from './views/HomePage.vue'
-import PostPage from './views/PostPage.vue'
-import PortfolioPage from './views/PortfolioPage.vue'
-import LoginPage from './views/LoginPage.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import HomePage from "./views/HomePage.vue";
+import PostPage from "./views/PostPage.vue";
+import PortfolioPage from "./views/PortfolioPage.vue";
+import LoginPage from "./views/LoginPage.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    scrollBehavior() {
-        return { x: 0, y: 0 };
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage
     },
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [{
-            path: '/',
-            name: 'home',
-            component: HomePage
-        },
-        {
-            path: '/post',
-            name: 'post',
-            component: PostPage
-        },
-        {
-            path: '/portfolio',
-            name: 'portfolio',
-            component: PortfolioPage
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: LoginPage
-        }
-    ]
-})
+    {
+      path: "/post",
+      name: "post",
+      component: PostPage
+    },
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: PortfolioPage
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPage
+    }
+  ]
+});
