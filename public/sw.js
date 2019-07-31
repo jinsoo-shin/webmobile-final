e244b94d7fc42c0bcfd85c0f2b8017f63fda70bc
신진수 <sjins0127@gmail.com>
2019-07-31 11:47:24 +0900

    첫 방문 알림허용설정 쿠키저장

--- public/sw.js
+++ public/sw.js
@@ -41,11 +41,7 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    // Return true if you want to remove this cache,
-                    // but remember that caches are shared across
-                    // the whole origin
-                }).map(function(cacheName) {
+                cacheNames.filter(function(cacheName) {}).map(function(cacheName) {
                     return caches.delete(cacheName);
                 })
             );
--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,26 +1,27 @@
 <template></template>
 
 <script>
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+// var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-window.onload = function () {
-            if (window.Notification) {
-                Notification.requestPermission();
-            }
-            notify();
-        }
-        function notify() {
-            if (Notification.permission !== 'granted') {
-                // alert('notification is disabled');
-                }
-            else {
-                if(!isChrome){
-                    alert('해당 사이트는 크롬에 최적화 되어 있습니다.');
-                    var notification = new Notification('해당 사이트는 크롬에 최적화 되어 있습니다.',{
-                        icon:'./chrome.png'
-                    });
-                }
+// window.onload = function () {
+//             if (window.Notification) {
+//                 Notification.requestPermission();
+//             }
+//             notify();
+//         }
+//         function notify() {
+//             if (Notification.permission !== 'granted') {
+//                 // alert('notification is disabled');
+//                 }
+//             else {
+//                 if()
+//                 if(!isChrome){
+//                     alert('해당 사이트는 크롬에 최적화 되어 있습니다.');
+//                     var notification = new Notification('해당 사이트는 크롬에 최적화 되어 있습니다.',{
+//                         icon:'./chrome.png'
+//                     });
+//                 }
  
-            }
-        }
+//             }
+//         }
 </script>
