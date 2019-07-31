b092624a83dbf802313988def8fca85c500208a1
Lee Jiseon <wiahawm7@gmail.com>
2019-07-31 16:26:41 +0900

    190731 | comment 기능 구현 중

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,23 +1,29 @@
+<template>
+    <div>
+        <div id="disqus_thread"></div>
+        Hello
+    </div>
+</template>
 
- <template>
- <div id="disqus_thread"></div>
- </template>
 <script>
-    var disqus_config = function () {
-        // this.page.url = '{{ site.url }}{{ page.url }}';  // Replace PAGE_URL with your page's canonical URL variable
-        // this.page.identifier = '5GisAwesome'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        var disqus_developer = 1;
-        };
-    
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://5gisawesome.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        })();
-</script>
-<script>
-export default {
-  name: 'Comment'
-}
+    export default {
+        mounted() {
+            console.log("Hello!")
+            try {
+                let disqus_config = function () {
+                    this.page.url = location.origin;
+                    this.page.identifier = location.pathname;
+                    console.log(this.page.url)
+                };
+                (function () {
+                    let d = document, s = d.createElement('script');
+                    s.src = 'https://happylovetkd.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            } catch (e) {
+                // some error
+            }
+        }
+    }
 </script>
