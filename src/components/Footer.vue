5e6ead3e429918622bb1e360e6dc0fbc7c529a75
hyunsang <happylovetkd@naver.com>
2019-08-06 15:27:21 +0900

    footer change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
         <v-layout hidden-xs-only>
           <v-flex sm4 offset-sm2>
             팀명 : 5G는 5조 <br> 프로젝트명 : webmobile-sub2 <br> 팀원 : 박성하, 신진수, 안현상, 이지선, 조성모 <br>
-            <a href="https://lab.ssafy.com/wiahawm/webmobile-sub2" target="_blank" style="color:rgb(043,063,107); font-weight:bold">
+            <a href="https://lab.ssafy.com/Leejiseon/webmobile-final" target="_blank" style="color:rgb(043,063,107); font-weight:bold">
                프로젝트 깃랩페이지 바로가기 <v-icon dark small color="rgb(043,063,107)">chevron_right</v-icon>
             </a>
           </v-flex>
